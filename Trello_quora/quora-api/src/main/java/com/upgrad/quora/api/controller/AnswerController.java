ba54e8e7f1a8b9afb221dfec3b5bcb867ffed4e4
Anil Narayanan <narayanan.anil@gmail.com>
2020-04-25 10:03:36 +0530

    Added comments to UserDAOImpl

--- Trello_quora/quora-api/src/main/java/com/upgrad/quora/api/controller/AnswerController.java
+++ Trello_quora/quora-api/src/main/java/com/upgrad/quora/api/controller/AnswerController.java
@@ -7,98 +7,136 @@ import com.upgrad.quora.service.exception.AnswerNotFoundException;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.InvalidQuestionException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/")
 public class AnswerController {
-    @Autowired
-    private AnswerService answerService;
+  @Autowired private AnswerService answerService;
 
-    /**
-     * @param authorization
-     * @param questionId
-     * @param answerRequest
-     * @return
-     * @throws AuthorizationFailedException
-     * @throws InvalidQuestionException
-     */
-    @RequestMapping(method= RequestMethod.POST, path="/question/{questionId}/answer/create", consumes= MediaType.APPLICATION_JSON_UTF8_VALUE, produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<AnswerResponse> createAnswer(@RequestHeader("authorization") final String authorization, @PathVariable("questionId") final String questionId, final AnswerRequest answerRequest) throws AuthorizationFailedException, InvalidQuestionException {
-        //Declaring the object of AnswerEntity class and set its corresponding attributes using answerRequest
-        AnswerEntity answerEntity = new AnswerEntity();
-        answerEntity.setAns(answerRequest.getAnswer());
-        answerEntity.setUuid(UUID.randomUUID().toString());
-        //After setting the attributes of answerEntity, pass it to createAnswer() method, to add this object to a persistent context
-        final AnswerEntity createdAnswer = answerService.createAnswer(questionId,authorization,answerEntity);
-        //Declaring an object of answerResponse type and set its attributes using createdAnswer
-        AnswerResponse answerResponse = new AnswerResponse().id(createdAnswer.getUuid()).status("ANSWER CREATED");
-        //Returns the answer UUID and Status in the Answer Response
-        return new ResponseEntity<AnswerResponse>(answerResponse, HttpStatus.CREATED);
-    }
+  /**
+   * @param authorization
+   * @param questionId
+   * @param answerRequest
+   * @return
+   * @throws AuthorizationFailedException
+   * @throws InvalidQuestionException
+   */
+  @RequestMapping(
+      method = RequestMethod.POST,
+      path = "/question/{questionId}/answer/create",
+      consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+      produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public ResponseEntity<AnswerResponse> createAnswer(
+      @RequestHeader("authorization") final String authorization,
+      @PathVariable("questionId") final String questionId,
+      final AnswerRequest answerRequest)
+      throws AuthorizationFailedException, InvalidQuestionException {
+    // Declaring the object of AnswerEntity class and set its corresponding attributes using
+    // answerRequest
+    AnswerEntity answerEntity = new AnswerEntity();
+    answerEntity.setAns(answerRequest.getAnswer());
+    answerEntity.setUuid(UUID.randomUUID().toString());
+    // After setting the attributes of answerEntity, pass it to createAnswer() method, to add this
+    // object to a persistent context
+    final AnswerEntity createdAnswer =
+        answerService.createAnswer(questionId, authorization, answerEntity);
+    // Declaring an object of answerResponse type and set its attributes using createdAnswer
+    AnswerResponse answerResponse =
+        new AnswerResponse().id(createdAnswer.getUuid()).status("ANSWER CREATED");
+    // Returns the answer UUID and Status in the Answer Response
+    return new ResponseEntity<AnswerResponse>(answerResponse, HttpStatus.CREATED);
+  }
 
-    /**
-     * @param authorization
-     * @param answerId
-     * @param answerEditRequest
-     * @return
-     * @throws AuthorizationFailedException
-     * @throws AnswerNotFoundException
-     */
-    @RequestMapping(method = RequestMethod.PUT, path="/answer/edit/{answerId}",consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<AnswerEditResponse> editAnswer(@RequestHeader("authorization") final String authorization, @PathVariable("answerId") final String answerId, final AnswerEditRequest answerEditRequest) throws AuthorizationFailedException, AnswerNotFoundException {
-        //Declaring the object of AnswerEntity class and set its corresponding attributes using answerRequest
-        AnswerEntity answerEntity = new AnswerEntity();
-        answerEntity.setAns(answerEditRequest.getContent());
-        //After setting the attributes of answerEntity, pass it to editAnswer() method, to add this object to a persistent context
-        final AnswerEntity editedAnswer = answerService.editAnswer(answerId,authorization,answerEntity);
-        //Declaring an object of answerEditResponse type and set its attributes using editedAnswer
-        AnswerEditResponse answerEditResponse = new AnswerEditResponse().id(editedAnswer.getUuid()).status("ANSWER EDITED");
-        //Returns the answer UUID and Status in the AnswerEditResponse
-        return new ResponseEntity<AnswerEditResponse>(answerEditResponse,HttpStatus.OK);
-    }
+  /**
+   * @param authorization
+   * @param answerId
+   * @param answerEditRequest
+   * @return
+   * @throws AuthorizationFailedException
+   * @throws AnswerNotFoundException
+   */
+  @RequestMapping(
+      method = RequestMethod.PUT,
+      path = "/answer/edit/{answerId}",
+      consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+      produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public ResponseEntity<AnswerEditResponse> editAnswer(
+      @RequestHeader("authorization") final String authorization,
+      @PathVariable("answerId") final String answerId,
+      final AnswerEditRequest answerEditRequest)
+      throws AuthorizationFailedException, AnswerNotFoundException {
+    // Declaring the object of AnswerEntity class and set its corresponding attributes using
+    // answerRequest
+    AnswerEntity answerEntity = new AnswerEntity();
+    answerEntity.setAns(answerEditRequest.getContent());
+    // After setting the attributes of answerEntity, pass it to editAnswer() method, to add this
+    // object to a persistent context
+    final AnswerEntity editedAnswer =
+        answerService.editAnswer(answerId, authorization, answerEntity);
+    // Declaring an object of answerEditResponse type and set its attributes using editedAnswer
+    AnswerEditResponse answerEditResponse =
+        new AnswerEditResponse().id(editedAnswer.getUuid()).status("ANSWER EDITED");
+    // Returns the answer UUID and Status in the AnswerEditResponse
+    return new ResponseEntity<AnswerEditResponse>(answerEditResponse, HttpStatus.OK);
+  }
 
-    /**
-     * @param authorization
-     * @param answerId
-     * @return
-     * @throws AuthorizationFailedException
-     * @throws AnswerNotFoundException
-     */
-    @RequestMapping(method = RequestMethod.DELETE, path="/answer/delete/{answerId}",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<AnswerDeleteResponse> editAnswer(@RequestHeader("authorization") final String authorization, @PathVariable("answerId") final String answerId) throws AuthorizationFailedException, AnswerNotFoundException {
-        final AnswerEntity deleteAnswer = answerService.deleteAnswer(answerId,authorization);
-        AnswerDeleteResponse answerDeleteResponse = new AnswerDeleteResponse().id(deleteAnswer.getUuid()).status("ANSWER DELETED");
-        //Returns the answer UUID of the deleted answer and Status in the AnswerDeleteResponse
-        return new ResponseEntity<AnswerDeleteResponse>(answerDeleteResponse,HttpStatus.OK);
-    }
+  /**
+   * @param authorization
+   * @param answerId
+   * @return
+   * @throws AuthorizationFailedException
+   * @throws AnswerNotFoundException
+   */
+  @RequestMapping(
+      method = RequestMethod.DELETE,
+      path = "/answer/delete/{answerId}",
+      produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public ResponseEntity<AnswerDeleteResponse> editAnswer(
+      @RequestHeader("authorization") final String authorization,
+      @PathVariable("answerId") final String answerId)
+      throws AuthorizationFailedException, AnswerNotFoundException {
+    final AnswerEntity deleteAnswer = answerService.deleteAnswer(answerId, authorization);
+    AnswerDeleteResponse answerDeleteResponse =
+        new AnswerDeleteResponse().id(deleteAnswer.getUuid()).status("ANSWER DELETED");
+    // Returns the answer UUID of the deleted answer and Status in the AnswerDeleteResponse
+    return new ResponseEntity<AnswerDeleteResponse>(answerDeleteResponse, HttpStatus.OK);
+  }
 
-    /**
-     * @param authorization
-     * @param questionId
-     * @return
-     * @throws AuthorizationFailedException
-     * @throws InvalidQuestionException
-     */
-    @RequestMapping(method = RequestMethod.GET, path="/answer/all/{questionId}",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<List<AnswerDetailsResponse>> getAllAnswers(@RequestHeader("authorization") final String authorization,@PathVariable("questionId") final String questionId)throws AuthorizationFailedException, InvalidQuestionException{
-        final List<AnswerEntity> receivedAllAnswers = answerService.getAllAnswers(questionId,authorization);
-        List<AnswerDetailsResponse> answerDetailsResponse = new ArrayList<AnswerDetailsResponse>();
-        for (AnswerEntity n : receivedAllAnswers) {
-            AnswerDetailsResponse answer = new AnswerDetailsResponse();
-            answer.id(n.getUuid()).answerContent(n.getAns()).questionContent(n.getQuestion().getContent());
-            answerDetailsResponse.add(answer);
-        }
-        //Returns the list of answers and Status in the AnswerDetailsResponse
-        return new ResponseEntity<List<AnswerDetailsResponse>> (answerDetailsResponse, HttpStatus.OK);
+  /**
+   * @param authorization
+   * @param questionId
+   * @return
+   * @throws AuthorizationFailedException
+   * @throws InvalidQuestionException
+   */
+  @RequestMapping(
+      method = RequestMethod.GET,
+      path = "/answer/all/{questionId}",
+      produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public ResponseEntity<List<AnswerDetailsResponse>> getAllAnswers(
+      @RequestHeader("authorization") final String authorization,
+      @PathVariable("questionId") final String questionId)
+      throws AuthorizationFailedException, InvalidQuestionException {
+    final List<AnswerEntity> receivedAllAnswers =
+        answerService.getAllAnswers(questionId, authorization);
+    List<AnswerDetailsResponse> answerDetailsResponse = new ArrayList<AnswerDetailsResponse>();
+    for (AnswerEntity n : receivedAllAnswers) {
+      AnswerDetailsResponse answer = new AnswerDetailsResponse();
+      answer
+          .id(n.getUuid())
+          .answerContent(n.getAns())
+          .questionContent(n.getQuestion().getContent());
+      answerDetailsResponse.add(answer);
     }
+    // Returns the list of answers and Status in the AnswerDetailsResponse
+    return new ResponseEntity<List<AnswerDetailsResponse>>(answerDetailsResponse, HttpStatus.OK);
+  }
 }
--- Trello_quora/quora-service/src/main/java/com/upgrad/quora/service/dao/UserDAOImpl.java
+++ Trello_quora/quora-service/src/main/java/com/upgrad/quora/service/dao/UserDAOImpl.java
@@ -13,11 +13,23 @@ public class UserDAOImpl implements UserDAO {
 
   @PersistenceContext private EntityManager entityManager;
 
-  public UserEntity signUpUser(UserEntity userEntity) {
+  /**
+   * Method to persist all the attributes about the user
+   *
+   * @param userEntity - Accepts UserEntity object as argument
+   * @return - returns userEntity
+   */
+  public UserEntity signUpUser(final UserEntity userEntity) {
     entityManager.persist(userEntity);
     return userEntity;
   }
 
+  /**
+   * Fetch user record by username
+   *
+   * @param userName - Username as argument
+   * @return - returns record as UserEntity or null
+   */
   public UserEntity getUserByName(final String userName) {
     try {
       return entityManager
@@ -29,6 +41,12 @@ public class UserDAOImpl implements UserDAO {
     }
   }
 
+  /**
+   * Fetch user record by email
+   *
+   * @param email - Email as argument
+   * @return - returns record as UserEntity or null
+   */
   public UserEntity getUserByEmail(final String email) {
     try {
       return entityManager
@@ -40,6 +58,12 @@ public class UserDAOImpl implements UserDAO {
     }
   }
 
+  /**
+   * Fetch user record by UUID
+   *
+   * @param userId - UUID as argument
+   * @return - returns record as UserEntity or null
+   */
   public UserEntity getUserByUuid(final String userId) {
     try {
       return entityManager
@@ -51,15 +75,29 @@ public class UserDAOImpl implements UserDAO {
     }
   }
 
+  /**
+   * Method to persist authentication token
+   *
+   * @param userAuthTokenEntity - Authorization token
+   * @return - Authorization token
+   */
   public UserAuthTokenEntity createAuthToken(final UserAuthTokenEntity userAuthTokenEntity) {
     entityManager.persist(userAuthTokenEntity);
     return userAuthTokenEntity;
   }
 
+  /** @param updatedUserEntity */
   public void updateUser(final UserEntity updatedUserEntity) {
     entityManager.merge(updatedUserEntity);
   }
 
+  /**
+   * Fetch user record
+   *
+   * @param userName - Username as argument
+   * @param password - Password as argument
+   * @return - returns record as UserEntity or null
+   */
   public UserEntity authenticateUser(final String userName, final String password) {
     try {
       return entityManager
@@ -72,6 +110,12 @@ public class UserDAOImpl implements UserDAO {
     }
   }
 
+  /**
+   * Fetch authorization token as record
+   *
+   * @param authorization - Authorization token
+   * @return - returns record as UserAuthTokenEntity
+   */
   public UserAuthTokenEntity getUserAuthToken(final String authorization) {
     try {
       return entityManager
@@ -83,6 +127,12 @@ public class UserDAOImpl implements UserDAO {
     }
   }
 
+  /**
+   * Sign out user
+   *
+   * @param userAuthTokenEntity - Authorization token
+   * @return - returns authorization token or null
+   */
   public UserAuthTokenEntity userSignOut(final UserAuthTokenEntity userAuthTokenEntity) {
     try {
       return entityManager.merge(userAuthTokenEntity);
@@ -91,7 +141,12 @@ public class UserDAOImpl implements UserDAO {
     }
   }
 
-  public void deleteUser(String uuid) {
+  /**
+   * Delete user with the matching UUID from related schemas
+   *
+   * @param uuid - UUID as argument
+   */
+  public void deleteUser(final String uuid) {
     UserEntity userEntity = getUserByUuid(uuid);
     entityManager.remove(userEntity);
   }
